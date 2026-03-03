ade56664e02187d090f4a203ad704e4deceb210a
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-03 23:17:53 +0600

    added an even condition with arrow function

--- arrow.js
+++ arrow.js
@@ -10,5 +10,9 @@ const div = function(num1, num2){
     const result = num1 / num2;
     return result;
 }
-console.log(div(4,2));
+// console.log(div(4,2));
 
+//arrow function 
+const isEven = a => a % 2 === 0;
+console.log(isEven(10));
+console.log(isEven(5));
