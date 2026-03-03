a5999058b7ca6b5b4f0b3798e2283c84cbd48512
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-03 23:25:56 +0600

    arrow function with multiline

--- arrow.js
+++ arrow.js
@@ -12,7 +12,19 @@ const div = function(num1, num2){
 }
 // console.log(div(4,2));
 
-//arrow function 
+//arrow function with single line
 const isEven = a => a % 2 === 0;
+ //arrow function a multiline likhle second bracet use korte hobe na
 console.log(isEven(10));
 console.log(isEven(5));
+
+//arrow function with multiline
+const doMath = (a, b) => {  // arrow function a multiline likhle second bracet use korte hobe
+    const div = a/b;
+    const sum = div + b;
+    const diff = a-b;
+    const mul = diff*a;
+    const twoTimes = mul*2;
+    return twoTimes;   // purotai return kora jabe na, specificvabe bole dite hobe j ki return korbo
+}
+console.log(doMath(2, 3));
